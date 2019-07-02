082dc476c757cfd67e6cd17c193e7ae521d34b55
Tijmen Elfrink <t.l.j.elfrink@students.uu.nl>
2019-07-02 16:07:54 +0200

    Added points to piece class, added type restrictions for class fields

--- Chess.py
+++ Chess.py
@@ -1,11 +1,20 @@
 class Piece:
+    color: chr
+    location: str
+    kind: str
+    points: int
+
     def __init__(self, color, start_location, kind):
-        self.color = color
-        self.location = start_location
-        self.kind = kind
+        self.color = color.upper()
+        self.location = start_location.upper()
+        self.kind = kind.upper()
+        self.points = self.set_points(kind.upper())
 
     def get_color(self):
         return self.color
+
+    def get_kind(self):
+        return self.kind
 
     def get_location(self):
         return self.location
@@ -13,8 +22,21 @@ class Piece:
     def move(self, new_location):
         self.location = new_location
 
+    def set_points(self, kind):
+        if kind == 'K':
+            return 100
+        if kind == 'Q':
+            return 9
+        if kind == 'N':
+            return 3
+        if kind == 'B':
+            return 3
+        if kind == 'R':
+            return 5
+        if kind == 'P':
+            return 1
 
-rook = Piece('w',)
+# rook = Piece('w',"a1",'r')
 
 class GameBoard:
     def __init__(self):
