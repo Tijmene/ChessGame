6853eaf7bb2b4f09e71b6c609eedec73d447dc9c
Tijmen Elfrink <t.l.j.elfrink@students.uu.nl>
2019-07-02 13:29:01 +0200

    Added very basic functions

--- Chess.py
+++ Chess.py
@@ -3,16 +3,20 @@ class Piece:
         self.color = color
         self.location = start_location
         self.kind = kind
-    def show(self):
-        print(self.color)
 
-    def move(self,new_location):
+    def get_color(self):
+        return self.color
+
+    def get_location(self):
+        return self.location
+
+    def move(self, new_location):
         self.location = new_location
 
-    def change_side(self):
-        if self.color == 'b':
-            self.color = 'w'
-        else:
-            self.color = 'b'
 
+rook = Piece('w',)
 
+currentPos = rook.get_location()
+
+class GameBoard:
+    def __init__(self):
