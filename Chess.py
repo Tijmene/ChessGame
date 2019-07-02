010ee6197feafa9b022a6c9f7d0e262538f382e8
Tijmen Elfrink <t.l.j.elfrink@students.uu.nl>
2019-07-02 17:22:47 +0200

    Added creation of initial board plus setting up the pieces

--- Chess.py
+++ Chess.py
@@ -36,7 +36,41 @@ class Piece:
         if kind == 'P':
             return 1
 
-# rook = Piece('w',"a1",'r')
+# rook = Piece('w',"A1",'r')
 
-class GameBoard:
+kind_dict = {'A':'R','B':'N','C':'B','D':'Q','E':'K','F':'R','G':'N','H':'B'}
+
+def initial_piece(file_rank):
+    if int(file_rank[1]) == 2 or int(file_rank[1]) == 7:
+        kind = 'P'
+    else:
+        kind = kind_dict[file_rank[0]]
+
+    if int(file_rank[1])<3:
+        piece = Piece('W' , file_rank, kind)
+    elif int(file_rank[1])>6:
+        piece = Piece('B' , file_rank, kind)
+    else:
+        return None
+    return piece
+
+
+class Game:
+    turn_counter: int
+
     def __init__(self):
+        self.game_board = self.create_game_board()
+
+    def create_game_board(self):
+        initial_board = dict()
+        for file in range(65,73):
+            for rank in range(1,9):
+                file_rank = chr(file) + str(rank)
+                initial_board[file_rank] = initial_piece(file_rank)
+        return initial_board
+
+    def get_game_board(self):
+        return self.game_board
+
+
+new_game = Game()
