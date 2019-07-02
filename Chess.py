e32ed5fca6bbfd29212196b14f6436c752ddf1f9
Tijmen Elfrink <t.l.j.elfrink@students.uu.nl>
2019-07-02 18:31:00 +0200

    Added location data type. They key of the board dict still uses the string representation and not the new data type: Future fix.

--- Chess.py
+++ Chess.py
@@ -1,12 +1,26 @@
+class Location:
+    file: str
+    rank: int
+
+    def __init__(self, file, rank):
+        self.file = file.upper()
+        self.rank = rank
+
+    def get_file(self):
+        return self.file
+
+    def get_rank(self):
+        return self.rank
+
 class Piece:
     color: chr
-    location: str
+    location: Location
     kind: str
     points: int
 
     def __init__(self, color, start_location, kind):
         self.color = color.upper()
-        self.location = start_location.upper()
+        self.location = start_location
         self.kind = kind.upper()
         self.points = self.set_points(kind.upper())
 
@@ -36,20 +50,20 @@ class Piece:
         if kind == 'P':
             return 1
 
-# rook = Piece('w',"A1",'r')
 
-kind_dict = {'A':'R','B':'N','C':'B','D':'Q','E':'K','F':'R','G':'N','H':'B'}
+kind_dict = {'A': 'R', 'B': 'N', 'C': 'B', 'D': 'Q', 'E': 'K', 'F': 'R', 'G': 'N', 'H': 'B'}
 
-def initial_piece(file_rank):
-    if int(file_rank[1]) == 2 or int(file_rank[1]) == 7:
+
+def initial_piece(location):
+    if location.get_rank() == 2 or location.get_rank() == 7:
         kind = 'P'
     else:
-        kind = kind_dict[file_rank[0]]
+        kind = kind_dict[location.get_file()]
 
-    if int(file_rank[1])<3:
-        piece = Piece('W' , file_rank, kind)
-    elif int(file_rank[1])>6:
-        piece = Piece('B' , file_rank, kind)
+    if location.get_rank() < 3:
+        piece = Piece('W', location, kind)
+    elif location.get_rank() > 6:
+        piece = Piece('B', location, kind)
     else:
         return None
     return piece
@@ -57,20 +71,38 @@ def initial_piece(file_rank):
 
 class Game:
     turn_counter: int
+    game_board: dict
 
     def __init__(self):
         self.game_board = self.create_game_board()
 
     def create_game_board(self):
         initial_board = dict()
-        for file in range(65,73):
-            for rank in range(1,9):
-                file_rank = chr(file) + str(rank)
-                initial_board[file_rank] = initial_piece(file_rank)
+        for file in range(65, 73):
+            for rank in range(1, 9):
+                initial_location = Location(chr(file), rank)
+                file_rank_key = chr(file) + str(rank)
+                initial_board[file_rank_key] = initial_piece(initial_location)
         return initial_board
 
     def get_game_board(self):
         return self.game_board
 
 
+# def create_move_vector(kind, location):
+#     if kind == 'R':
+#
+#     elif kind == 'K':
+#
+#     elif kind == 'B':
+#
+#     elif kind == 'Q':
+#
+#     elif kind == 'K':
+#
+#     elif kind == 'P':
+
+
 new_game = Game()
+
+x = 3
