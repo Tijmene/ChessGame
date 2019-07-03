d3289615dead485bc846039389c3ae96eaf0c8f6
Tijmen Elfrink <t.l.j.elfrink@students.uu.nl>
2019-07-03 17:15:33 +0200

    The board is build and the pieces are setup. The code is really ugly but I could not get it to work otherwise. If you run GUI.py you will see the board with the pieces.

--- GUI.py
+++ GUI.py
@@ -3,7 +3,7 @@ import tkinter as tk
 import PIL.Image
 import PIL.ImageTk
 from PIL import Image
-
+import time
 
 class GameBoard(tk.Frame):
     def __init__(self, parent, rows=8, columns=8, size=32, color1="white", color2="blue"):
@@ -64,15 +64,113 @@ class GameBoard(tk.Frame):
     def get_size(self):
         return self.size
 
-if __name__ == "__main__":
-    im = PIL.Image.open("./icons/WhitePawn.png")
+
+def generate_game_board():
     root = tk.Tk()
     board = GameBoard(root)
     piece_size = int(board.get_size() * 0.8)
-    im = im.resize((piece_size, piece_size), Image.ANTIALIAS)
-
     board.pack(side="top", fill="both", expand="true", padx=4, pady=4)
-    player1 = PIL.ImageTk.PhotoImage(im)
-    # player1.subsample(25)
-    board.addpiece("player1", player1, 2, 0)
+
+    color = "B"
+    kind = "P"
+    image = PIL.Image.open("./icons/" + color + kind + ".PNG")
+    image = image.resize((piece_size, piece_size), Image.ANTIALIAS)
+    piece1 = PIL.ImageTk.PhotoImage(image)
+    for rank in range(8):
+        name = color + kind + str(rank)
+        board.addpiece(name, piece1, 1, rank)
+
+    kind = "R"
+    image = PIL.Image.open("./icons/" + color + kind + ".PNG")
+    image = image.resize((piece_size, piece_size), Image.ANTIALIAS)
+    piece2 = PIL.ImageTk.PhotoImage(image)
+    for rank in "07":
+        name = color + kind + rank
+        board.addpiece(name, piece2, 0, int(rank))
+
+    kind = "N"
+    image = PIL.Image.open("./icons/" + color + kind + ".PNG")
+    image = image.resize((piece_size, piece_size), Image.ANTIALIAS)
+    piece3 = PIL.ImageTk.PhotoImage(image)
+    for rank in "16":
+        name = color + kind + rank
+        board.addpiece(name, piece3, 0, int(rank))
+
+    kind = "B"
+    image = PIL.Image.open("./icons/" + color + kind + ".PNG")
+    image = image.resize((piece_size, piece_size), Image.ANTIALIAS)
+    piece4 = PIL.ImageTk.PhotoImage(image)
+    for rank in "25":
+        name = color + kind + rank
+        board.addpiece(name, piece4, 0, int(rank))
+
+    kind = "Q"
+    image = PIL.Image.open("./icons/" + color + kind + ".PNG")
+    image = image.resize((piece_size, piece_size), Image.ANTIALIAS)
+    piece5 = PIL.ImageTk.PhotoImage(image)
+    rank = "3"
+    name = color + kind + rank
+    board.addpiece(name, piece5, 0, int(rank))
+
+    kind = "K"
+    image = PIL.Image.open("./icons/" + color + kind + ".PNG")
+    image = image.resize((piece_size, piece_size), Image.ANTIALIAS)
+    piece6 = PIL.ImageTk.PhotoImage(image)
+    rank = "4"
+    name = color + kind + rank
+    board.addpiece(name, piece6, 0, int(rank))
+
+    color = "W"
+    kind = "P"
+    image = PIL.Image.open("./icons/" + color + kind + ".PNG")
+    image = image.resize((piece_size, piece_size), Image.ANTIALIAS)
+    piece7 = PIL.ImageTk.PhotoImage(image)
+
+    for rank in range(8):
+        name = color + kind + str(rank)
+        board.addpiece(name, piece7, 6, rank)
+
+    kind = "R"
+    image = PIL.Image.open("./icons/" + color + kind + ".PNG")
+    image = image.resize((piece_size, piece_size), Image.ANTIALIAS)
+    piece8 = PIL.ImageTk.PhotoImage(image)
+    for rank in "07":
+        name = color + kind + rank
+        board.addpiece(name, piece8, 7, int(rank))
+
+    kind = "N"
+    image = PIL.Image.open("./icons/" + color + kind + ".PNG")
+    image = image.resize((piece_size, piece_size), Image.ANTIALIAS)
+    piece9 = PIL.ImageTk.PhotoImage(image)
+    for rank in "16":
+        name = color + kind + rank
+        board.addpiece(name, piece9, 7, int(rank))
+
+    kind = "B"
+    image = PIL.Image.open("./icons/" + color + kind + ".PNG")
+    image = image.resize((piece_size, piece_size), Image.ANTIALIAS)
+    piece10 = PIL.ImageTk.PhotoImage(image)
+    for rank in "25":
+        name = color + kind + rank
+        board.addpiece(name, piece10, 7, int(rank))
+
+    kind = "Q"
+    image = PIL.Image.open("./icons/" + color + kind + ".PNG")
+    image = image.resize((piece_size, piece_size), Image.ANTIALIAS)
+    piece11 = PIL.ImageTk.PhotoImage(image)
+    rank = "3"
+    name = color + kind + rank
+    board.addpiece(name, piece11, 7, int(rank))
+
+    kind = "K"
+    image = PIL.Image.open("./icons/" + color + kind + ".PNG")
+    image = image.resize((piece_size, piece_size), Image.ANTIALIAS)
+    piece12 = PIL.ImageTk.PhotoImage(image)
+    rank = "4"
+    name = color + kind + rank
+    board.addpiece(name, piece12, 7, int(rank))
     root.mainloop()
+
+
+if __name__ == "__main__":
+    generate_game_board()
