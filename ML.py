cd8dcf025fa428a16fa7c990e464ea8a3ca5822a
Tijmen Elfrink <tijmenelfrink@gmail.com>
2019-09-26 14:18:15 +0200

    Changed something in the ML file

--- ML.py
+++ ML.py
@@ -18,3 +18,20 @@ def create_ml_input(state_board):
     ml_input = np.array(ml_input)
     return ml_input
 
+
+def create_ml_input_piece_moved(state_board):
+    state = state_board.get_game_state()
+    ml_input = []
+    for file in range(65, 73):
+        for rank in range(1, 9):
+            element = state[chr(file) + str(rank)]
+            if element is not None:
+                encoding = 1
+            else:
+                encoding = 0
+            encoding = np.array(encoding)
+            ml_input.append(encoding)
+    ml_input = np.array(ml_input)
+    return ml_input
+
+
